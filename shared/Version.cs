acac3b54602abc5ae9fc187680b15a26d5d86768
CodeSavvyGeek <codesavvygeek@gmail.com>
2012-05-07 20:30:20 -0400

    added shared files

---
+++ shared/Version.cs
@@ -0,0 +1,19 @@
+﻿/*
+ *	Common version information for C# projects
+ *
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !!!!!!!!!!!! THIS FILE IS SHARED !!!!!!!!!!!!!!!!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System;
+
+[assembly: AssemblyCompany("KSquared")]
+[assembly: AssemblyProduct("Fantasy Sports Coach")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
