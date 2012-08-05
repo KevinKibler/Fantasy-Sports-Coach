49f0bccd554d9048b981a0f611da7d0a4ef045dc
CodeSavvyGeek <codesavvygeek@gmail.com>
2012-08-05 00:05:08 -0400

    added website project and tests

---
+++ apps/Website/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FantasySportsCoach.Models;
+
+namespace FantasySportsCoach.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.ProjectUrl = @"https://github.com/CodeSavvyGeek/Fantasy-Sports-Coach";
+			return View();
+		}
+	}
+}
---
+++ apps/Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FantasySportsCoach.MvcApplication" Language="C#" %>
